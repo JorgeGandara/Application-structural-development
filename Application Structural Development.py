31a836d102c14925787fd1f3cf6f540d2ab154d9
Jorge <gandara07@outlook.es>
2023-03-22 22:29:16 -0500

    Ahora si, este SÍ es el archivo con las clases principales y sus atributos (el anterior era una prueba que estaba haciendo mientras aprendía a usar github)

--- Application Structural Development.py
+++ Application Structural Development.py
@@ -1,7 +1,28 @@
-num1 = float(input("Ingrese el primer número: "))
-num2 = float(input("Ingrese el segundo número: "))
-num3 = float(input("Ingrese el tercer número: "))
-
-promedio = (num1 + num2 + num3) / 3
-
-print("El promedio es:", promedio)
+# Jorge Andres Gandara Oliveros T00065470
+class Package:
+    id = int
+    weight = 0
+    description = str
+    cost = float
+    W_GR_100 = 1
+class Adress:
+    st = str
+    city = str
+    hood = str
+    country = str
+    PC = int
+class Person:
+    name=str
+    id=int
+    adress = [Adress]
+    phone = int
+class Deliver:
+    id=int
+    date=int
+    time=int
+    sender=Person
+    reciver=Person
+    sender_add=Adress
+    reciver_add=Adress
+    contact=Person
+    items=[Package]
